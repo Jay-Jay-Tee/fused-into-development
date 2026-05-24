cdd996e7257de733a5e32245913daf054dbb1009
Satrajit <satrajitmondal06@gmail.com>
2026-05-24 17:43:46 +0530

    added add and list pages

--- vendor/src/assets/assets.js
+++ vendor/src/assets/assets.js
@@ -1,7 +1,52 @@
 import logo from './logo.png'
 import parcel_icon from './parcel_icon.svg'
+import upload_area from './upload_area.png'
 
 export const assets = {
     logo,
     parcel_icon,
+    upload_area,
 }
+
+export const products = [
+    {
+        _id: 'v_p001',
+        name: 'Cotton handloom kurta',
+        image: ['https://picsum.photos/seed/v001/300/400'],
+        category: 'Clothing',
+        price: 1299,
+        stock: 24,
+    },
+    {
+        _id: 'v_p002',
+        name: 'Brass desk lamp',
+        image: ['https://picsum.photos/seed/v002/300/400'],
+        category: 'Home',
+        price: 2499,
+        stock: 3,
+    },
+    {
+        _id: 'v_p003',
+        name: 'Leather wallet, slim',
+        image: ['https://picsum.photos/seed/v003/300/400'],
+        category: 'Accessories',
+        price: 1799,
+        stock: 31,
+    },
+    {
+        _id: 'v_p004',
+        name: 'Ceramic pour-over set',
+        image: ['https://picsum.photos/seed/v004/300/400'],
+        category: 'Home',
+        price: 1899,
+        stock: 1,
+    },
+    {
+        _id: 'v_p005',
+        name: 'Khadi cotton shirt',
+        image: ['https://picsum.photos/seed/v005/300/400'],
+        category: 'Clothing',
+        price: 1599,
+        stock: 18,
+    },
+]
